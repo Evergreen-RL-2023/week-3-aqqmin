debdba89a8b4181b0fb8ea7ee3b2d9defab1342d
aqqmin <n.quinnallen@gmail.com>
2023-04-20 19:22:21 +0000

    doing things

--- rl_grid_world.py
+++ rl_grid_world.py
@@ -1,3 +1,5 @@
+### NICOLAS QUINN ALLEN
+
 '''
 template: Richard Weiss, April 2023
 grid world simulation 
@@ -23,27 +25,53 @@ import random as rnd
 
 N = 5   #grid size
 
+class Agent():
+    def __init__(self,r,c):
+        self.row = r
+        self.col = c
+        self.gridcell = null
+        self.rewards = 0
+        self.actions = ['n','e','s','w']
 
+    def step():
+        act_index = rnd.randrange(0, 4)
+        action = self.actions[act_index]
+        self.rewards += get_reward(gridcell, action)
+
+    def get_reward(gridcell):
+        if ((gridcell.row == 0 and action == 'n') or (gridcell.col == 0 and action == 'w') or (gridcell.row == gridworld.size and action == 's') or (gridcell.col == gridworld.size and action == 'e')):
+            reward = -1
+        else:
+            reward = gridcell.reward
+        return reward
+
+class GridWorld():
+    def __init__(self, size):
+        self.grid = []
+        for i in range(size):
+            print(i)
+            self.grid.append([])
+            for k in range(size):
+                self.grid[i].append(GridCell(i,k))
+                #print(f'creating: {i},{k}')
 
 class GridCell():
     def __init__(self, r, c):
-        actions = ['n', 'e', 'w', 's']
         # what else do you need?
-        row = r
-        col = c
-
-
-    def get_reward(action):
-        if row == 0 and action == 's':
-            reward = 2
-        else:
-            reward = 0
-        return reward
-
+        self.row = r
+        self.col = c
+        self.type = 0
+        if self.type == 0:
+            self.reward = 0
+        if self.type == 1:
+            self.reward = 10
+        if self.type ==2:
+            self.reward = 5
     
-    def get_next_cell(action):
-        r = row
-        c = col
+    def get_next_cell(self, action):
+        r = self.row
+        c = self.col
+        print(f'row {r} , col {c}')
         # action is 'n', 'e', 's', 'w'
         '''
         match action:
@@ -64,10 +92,15 @@ class GridCell():
 
 if __name__ == '__main__':
     rnd.seed(42)
-    grid = []
-
+    
+    GridWorld(N)
+    
+    
+    '''grid = []
+    gcell = GridCell(1,1)
+    gcell.get_next_cell('n')
     for i in range(N):
         grid.append([])
         for j in range(N):
             grid[i].append(GridCell(i,j))
-            print(grid[i][j], end=' ')
+            print(grid[i][j], end=' ')'''
