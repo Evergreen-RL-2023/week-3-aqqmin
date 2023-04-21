80ed2773ed996d910e3e6ac0e27cd85022150cdc
aqqmin <n.quinnallen@gmail.com>
2023-04-21 02:39:32 +0000

    have now gotten this agent traversing and learning maybe not the way we meant to make it work, but it does work to some degree

--- rl_grid_world.py
+++ rl_grid_world.py
@@ -26,75 +26,113 @@ import random as rnd
 N = 5   #grid size
 
 class Agent():
-    def __init__(self,r,c):
+    def __init__(self,r,c,gridworld):
         self.row = r
         self.col = c
-        self.gridcell = null
+        self.gridcell = gridworld.grid[r][c]
         self.rewards = 0
         self.actions = ['n','e','s','w']
 
-    def step():
+    def status(self):
+        print(f'I am at {self.row}, {self.col}')
+        return self.row, self.col
+
+    def step(self):
         act_index = rnd.randrange(0, 4)
         action = self.actions[act_index]
-        self.rewards += get_reward(gridcell, action)
+        best_value = max(self.gridcell.vals, key=self.gridcell.vals.get)
+        if (rnd.randrange(0,1)>.25):
+            action = best_value
+        self.rewards += self.traverse(action)
 
-    def get_reward(gridcell):
-        if ((gridcell.row == 0 and action == 'n') or (gridcell.col == 0 and action == 'w') or (gridcell.row == gridworld.size and action == 's') or (gridcell.col == gridworld.size and action == 'e')):
-            reward = -1
-        else:
-            reward = gridcell.reward
-        return reward
+
+    def traverse(self, action):
+        print(f"traversing {action} !")
+        reward = self.gridcell.get_reward(action)
+        self.gridcell.vals[action] = (self.gridcell.vals[action] + reward[0])/2
+        self.gridcell = reward[1]
+        self.gridcell.visits += 1
+        return reward[0]
+        
 
 class GridWorld():
-    def __init__(self, size):
+    def __init__(self, size, agentR, agentC):
         self.grid = []
+        self.size = size
         for i in range(size):
             print(i)
             self.grid.append([])
             for k in range(size):
-                self.grid[i].append(GridCell(i,k))
-                #print(f'creating: {i},{k}')
+                self.grid[i].append(GridCell(i,k,self))
+        self.agent = Agent(agentR,agentC,self)
+        self.agent.status()
 
 class GridCell():
-    def __init__(self, r, c):
+    def __init__(self, r, c, gridworld):
         # what else do you need?
+        self.gridworld = gridworld
         self.row = r
         self.col = c
+        self.vals = {'n':1,'e':1,'s':1,'w':1}
+        self.reward = 0
         self.type = 0
-        if self.type == 0:
-            self.reward = 0
+        self.visits = 0
+
+        if self.row == 0 and self.col == 1:
+            self.type = 1
+            print("Cell A now exists")
+        if self.row == 0 and self.col == 3:
+            self.type = 2
+            print("Cell B now exists")
+
         if self.type == 1:
             self.reward = 10
-        if self.type ==2:
+        elif self.type == 2:
             self.reward = 5
-    
-    def get_next_cell(self, action):
-        r = self.row
-        c = self.col
-        print(f'row {r} , col {c}')
-        # action is 'n', 'e', 's', 'w'
-        '''
-        match action:
-           case 'n':
-              r -= 1
-           case _:
-              print(action, 'invalid action')
-        '''
 
-        return r,c
-    
+    def get_reward(self, action):
+        if ((self.row == 0 and action == 'n') or (self.col == 0 and action == 'w') or (self.row == (self.gridworld.size - 1) and action == 's') or (self.col == (self.gridworld.size - 1) and action == 'e')):
+            reward = -2
+            next_cell = self
 
+        else:
+            if self.type == 1: 
+                next_cell = self.gridworld.grid[4][1]
+            if self.type == 2:
+                next_cell = self.gridworld.grid[2][3]
+            else:
+                match action:
+                    case 'n':
+                        next_cell = self.gridworld.grid[self.row-1][self.col]
+                    case 'e':
+                        next_cell = self.gridworld.grid[self.row][self.col+1]
+                    case 's':
+                        next_cell = self.gridworld.grid[self.row+1][self.col]
+                    case 'w':
+                        next_cell = self.gridworld.grid[self.row][self.col-1]
+            reward = next_cell.reward
+            #get reward uppn entering cell, this will allow to map action values for each action from each cell
+        return reward, next_cell    
+
+''' 
     def step():
         act_index = rnd.randrange(0, 4)
         action = actions[act_index]
         rwd = get_reward(action)
-
+'''
 
 if __name__ == '__main__':
     rnd.seed(42)
     
-    GridWorld(N)
-    
+    world = GridWorld(N,N//2,N//2)
+    s = 0
+    while(s<10000):
+        world.agent.step()
+        s+=1
+    for i in range (world.size):
+        print(f"\n printing row {i}'s visits and values: ")
+        for j in range (world.size):
+            print(f"cell {i},{j} visited {world.grid[i][j].visits} times \n agent knows :{world.grid[i][j].vals}")
     
     '''grid = []
     gcell = GridCell(1,1)
